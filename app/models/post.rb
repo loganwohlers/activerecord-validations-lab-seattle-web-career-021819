77ceba612b504f18d8fcf67094cd3ae7c3e33c22
loganwohlers <lawohlers@gmail.com>
2019-03-20 09:26:35 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence:true
+    validate :post_title
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Nonfiction)} 
+   
+    def post_title
+        if !self.title.nil?
+          phrases = ["Won\'t Believe", "Secret", "Top", "Guess"]
+          if !phrases.any? {|phrase| self.title.include?(phrase)}
+            errors.add(:title, "Not enough!")
+          end
+        else
+          errors.add(:title, "Not enough!")
+        end
+    end
 end
